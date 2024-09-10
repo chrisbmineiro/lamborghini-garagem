fc75e0c8e8fe2214bcb2c35f00cb37715a75f2b4
chrisbmineiro <chrisbmineiro@gmail.com>
2024-09-10 18:47:14 -0300

    adicionando API services e finalizando projeto

--- src/components/CardView/CardView.tsx
+++ src/components/CardView/CardView.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, Text, Button } from "react-native";
 import { styles } from "./CardViewStyles";
 import logo from "../../../assets/logo.png";
-import { CAT_ASSETS_BASE_URL } from "../../constants/car";
+import { CAR_ASSETS_BASE_URL } from "../../constants/car";
 import Divider from "../Divider/Divider";
 import BuyButton from "../BuyButton/BuyButton";
+import { CarModel } from "./props";
+import { handleNextItem, handlePreviousItem, loadCarData } from "./actions";
 
 export default function CardView() {
+  const [carData, setCarData] = useState<CarModel | null>(null);
+  useEffect(() => {
+    (async () => {
+      await loadCarData(1, setCarData);
+    })();
+  }, []);
   // sub-componente para renderizar o logo
   const renderLogo = () => {
     return (
@@ -19,8 +27,8 @@ export default function CardView() {
   const renderCarDetails = () => {
     return (
       <View style={{ alignItems: "center" }}>
-        <Text style={styles.carBrand}>Lamborgha</Text>
-        <Text style={styles.carModel}>Ferrari</Text>
+        <Text style={styles.carBrand}>{carData?.releaseYear}</Text>
+        <Text style={styles.carModel}>{carData?.carName}</Text>
       </View>
     );
   };
@@ -28,7 +36,7 @@ export default function CardView() {
   const renderCarImage = () => {
     return (
       <Image
-        source={{ uri: `${CAT_ASSETS_BASE_URL}1.png` }}
+        source={{ uri: `${CAR_ASSETS_BASE_URL}${carData?.id}.png` }}
         style={styles.image}
       />
     );
@@ -37,9 +45,9 @@ export default function CardView() {
   const renderPrice = () => {
     return (
       <View style={styles.priceControls}>
-        <Button onPress={()=>{}} title="<" color={"#01A6B3"} />
-        <Text style={styles.price}>Valor</Text>
-        <Button onPress={()=>{}} title=">" color={"#01A6B3"} />
+        <Button onPress={()=> handlePreviousItem(carData, setCarData)} title="<" color={"#01A6B3"} />
+        <Text style={styles.price}>{carData?.price}</Text>
+        <Button onPress={()=> handleNextItem(carData, setCarData)} title=">" color={"#01A6B3"} />
       </View>
     );
   };
